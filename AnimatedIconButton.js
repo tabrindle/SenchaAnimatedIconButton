531dc10dffe06230c1d381bb14a08f1b615d178f
Trevor Brindle <trevorb@marketamerica.com>
2015-04-15 15:58:21 -0400

    Initial Commit

---
+++ AnimatedIconButton.js
@@ -0,0 +1,76 @@
+Ext.define('Ext.ux.AnimatedIconButton', {
+    extend: 'Ext.Component',
+
+    xtype: 'AnimatedIconButton',
+
+    template: [
+    	{
+    		tag: 'div',
+    		className: Ext.baseCSSPrefix + 'animated-icon-element-container',
+    		reference: 'iconElementContainer',
+    		children: [
+    			{
+		            tag: 'div',
+		            className: Ext.baseCSSPrefix + 'icon-element-top',
+		            reference: 'iconElementTop'
+		        },
+		        {
+		            tag: 'div',
+		            className: Ext.baseCSSPrefix + 'icon-element-bottom',
+		            reference: 'iconElementBottom'
+		        }
+    		]
+    	}
+    ],
+
+    initialize: function() {
+        this.callParent();
+
+        this.element.on({
+            scope      : this,
+            tap        : 'onTap'
+        });
+    },
+
+    onTap: function(e) {
+    	console.log('onTap', arguments)
+
+    	if(this.iconElementContainer.hasCls(Ext.baseCSSPrefix + 'animated-icon-element-container')){
+    		this.iconElementContainer.replaceCls(Ext.baseCSSPrefix + 'animated-icon-element-container', Ext.baseCSSPrefix + 'animated-icon-element-container-2');
+    	} else {
+    		this.iconElementContainer.replaceCls(Ext.baseCSSPrefix + 'animated-icon-element-container-2', Ext.baseCSSPrefix + 'animated-icon-element-container');
+    	}
+
+    	if(this.iconElementBottom.hasCls(Ext.baseCSSPrefix + 'icon-element-bottom')){
+    		this.iconElementBottom.replaceCls(Ext.baseCSSPrefix + 'icon-element-bottom', Ext.baseCSSPrefix + 'icon-element-bottom-2');
+    		this.iconElementTop.replaceCls(Ext.baseCSSPrefix + 'icon-element-top', Ext.baseCSSPrefix + 'icon-element-top-2');
+    	} else {
+    		this.iconElementBottom.replaceCls(Ext.baseCSSPrefix + 'icon-element-bottom-2', Ext.baseCSSPrefix + 'icon-element-bottom');
+    		this.iconElementTop.replaceCls(Ext.baseCSSPrefix + 'icon-element-top-2', Ext.baseCSSPrefix + 'icon-element-top');
+    	}
+
+    	this.fireAction('tap', [this, e], 'doTap');
+    },
+  
+    /**
+     * @private
+     */
+    doTap: function(me, e) {
+        var handler = me.getHandler(),
+            scope = me.getScope() || me;
+
+        if (!handler) {
+            return;
+        }
+
+        if (typeof handler == 'string') {
+            handler = scope[handler];
+        }
+
+        if (e && e.preventDefault) {
+            e.preventDefault();
+        }
+
+        handler.apply(scope, arguments);
+    }
+});
